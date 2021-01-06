abe4a33414e05894b570ec8ef090f9e6e7dc9166
PHoogestraat <68931021+PHoogestraat@users.noreply.github.com>
2021-01-05 23:34:25 -0500

    buble chart added. Does not work

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,34 +1,40 @@
-// Taken from Activity 2 exercise 3
+// 
 //const url = "samples.json"
-var samples = 940
+//var samples = 940
 //function buildPlot() {
-    // Fetch the JSON data and console log it
+    // Fetch the JSON data and send to console log. This will contoin entire data set
     d3.json("samples.json").then(function(data) {
         console.log(data);
+    // appends name:id to html <select> and appeds each id number as an option
         data.names.forEach(i => 
           d3.select("select").append("option").text(i )
           );
-          var selectiud = d3.select("#selDataset").property("value");
-          
-          d3.select("#selDataset").on("change", buildPlot(selectiud));
+          // pulls value from pull down menu
+          // ****************************************************
+          // this is the hangup!!!!!!!! selectID is set to 940 = sample
+          var selectID = d3.select("#selDataset").property("value");
+          // exacutes buildPlot function based on selectID input
+          // selectID = samples (which is static 940)
+          // problem is may also be here 
+          d3.select("#selDataset").on("change", buildPlot(selectID));
         
-        // Grab values from the data json object to build the plots
-      
+          console.log(`select id value: ${selectID}`);
+         
       });
+      
+// sets up bar plot
 function buildPlot(samples){ 
         d3.select ("#bar").html("") 
         d3.json("samples.json").then(function(data) {
         var sampleValues = data.samples.filter (i => i.id == samples)[0];
-        //var otuIDS = data.samples.map(i => [i.otuIDS]);
-        // bacteria name----hovering text
-        //var outLabels = samples.otu_labels;
+
         console.log(sampleValues);
-        
+        console.log(`samples value: ${samples}`);
         //
     var otuIDS = sampleValues.otu_ids.slice(0, 10).reverse();
     var sample_X = sampleValues.sample_values.slice(0, 10).reverse();
     console.log(otuIDS);
-        // Trace1 for the Greek Data
+        // Trace1 for bar graph
     var trace1 = {
       x: sample_X,
       y: otuIDS.map(i=> `otu ${i}`),
@@ -43,7 +49,7 @@ function buildPlot(samples){
 
     // Apply the group bar mode to the layout
     var layout = {
-      title: "Top 10 OTUs",
+      title: "Top 10 OTUs for selected ID No",
       margin: {
         l: 100,
         r: 100,
@@ -55,6 +61,31 @@ function buildPlot(samples){
     // Render the plot to the div tag with id "plot"
     Plotly.newPlot("bar", data, layout);
 
-        })
-  };
+          })
+
+      // The bubble chart
+      
+      var trace1 = {
+        x: samples.otu_ids,
+        y: sampleValues.sample_values,
+        mode: 'markers',
+        marker: {
+          size: sampleValues.sample_values
+        }
+      };
+      
+      var data = [trace1];
+      
+      var layout = {
+        title: 'Marker Size',
+        showlegend: false,
+        height: 600,
+        width: 600
+      };
+
+    Plotly.newPlot('bubble', data, layout);
+                      
+    
+    console.log(`sample_values:${sampleValues.sample_values}`);
+};
         
