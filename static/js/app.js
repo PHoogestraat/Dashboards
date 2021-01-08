0fab9e6997ec74c26c7ef2891586386e570f4852
PHoogestraat <68931021+PHoogestraat@users.noreply.github.com>
2021-01-07 21:41:47 -0500

    closer

--- static/js/app.js
+++ static/js/app.js
@@ -20,24 +20,27 @@ d3.json("samples.json").then(function(data) {
       dropdown.on("change", plot(selectID)); 
       dropdown.on("change", getInfo(selectID));
       
-      console.log(`id3: ${id}`);
+      
 
 });
 
 //###############################################################
     // create the function to get the necessary data
 function getInfo(id) {
-  // read the json file to get data
-  d3.json("data/samples.json").then((data)=> {
+  // read the json file to get data and creartes an function data1 that filters data
+  d3.json("data/samples.json").then(function(data1) {
       
-        // get the metadata info for the demographic panel
-        var metadata = data.metadata;
+        // Get the metadata info for the demographic panel
+        var metadata = data1.metadata;
 
-        console.log(metadata)
-
-        // filter meta data info by id
-        var result = metadata.filter(meta => meta.id.toString() === id)[0];
-
+        //console.log(metadata);
+        
+        console.log(`id3: ${id}`);
+        // filter meta data info by id PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP
+        var result = metadata.filter(meta => meta.id.toString() == id)[0];
+        
+        console.log(`meta3: ${result}`);
+        
         // select demographic panel to put data
         var demographicInfo = d3.select("#sample-metadata");
         
@@ -46,117 +49,88 @@ function getInfo(id) {
 
         // grab the necessary demographic data data for the id and append the info to the panel
         Object.entries(result).forEach((key) => {   
-                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+                demographicInfo.append("h5").text(key[0] + ": " + key[1] + "\n");    
       });
   });
 }
 //#########################################################################
 
-
-
-
-
-// get metadata for id
-function mData(metaID){
-
-    
-    // Grab values from the data json object to build the plots
-    var id = data.metadata.metaID.id[0];
-    //var ethnicity = mData.metadata.ethnicity;
-    console.log(`id1: ${id}`);
-    //var startDate = mData.metadata.gender;
-    //var location = mData.metadata.location;
-    //var bbtype = mData.metadata.bbtype;
-    //var wfreq = mData.metadata.wfreq;
-    // Associate data from above with key pair below
-    d3.select("#sample-metadata").append("h6").text(id);
-    //d3.select("#sample-metadata").append("panel-body").text(ethnicity);
-    //d3.select("#sample-metadata").append("panel-body").text(startDate);
-    //d3.select("#sample-metadata").append("panel-body").text(location);
-    //d3.select("#sample-metadata").append("panel-body").text(bbtype);
-    //d3.select("#sample-metadata").append("panel-body").text(wfreq);
-
-    // ##### html target where data above must be placed
-    console.log(`id2: ${id}`);
-    //<div id="sample-metadata" class="panel-body"></div>    
-  };
-  
-  // create the function to get the necessary data
-
-
-
 // sets up bar plot
 function plot(samples){ 
-  d3.select ("#bar").html("") 
-  d3.json("samples.json").then(function(data) {
-    var sampleValues = data.samples.filter (i => i.id == samples)[0];
+  // 
+  //d3.select ("#bar").html("") ######### Don't need. Target identified in plot
+  // read the json file to get data: SampleData or "samples"
+    d3.json("samples.json").then(function(data) {
 
-    console.log(sampleValues);
-    console.log(`samples value: ${samples}`);
+        // filters data from samples by using arrow loop i function
+        var sampleValues = data.samples.filter (i => i.id == samples)[0];
 
-        //x any y plots for bar
-    var otuIDS = sampleValues.otu_ids.slice(0, 10).reverse();
-    var sample_X = sampleValues.sample_values.slice(0, 10).reverse();
-    var prNames = sampleValues.otu_labels.slice(0,10).reverse();
-    console.log(otuIDS);
+        console.log(sampleValues);
+        console.log(`samples value: ${samples}`);
+
+            //x any y plots for bar
+        var otuIDS = sampleValues.otu_ids.slice(0, 10).reverse();
+        var sample_X = sampleValues.sample_values.slice(0, 10).reverse();
+        var prNames = sampleValues.otu_labels.slice(0,10).reverse();
+        console.log(otuIDS);
 
 
-        // Trace1 for bar graph
-    var trace1 = {
-      x: sample_X,
-      y: otuIDS.map(i=> `otu ${i}`),
-      text: sampleValues.otu_labels.slice(0,10).reverse(),
-      name: "Top 10 OTUs",
-      type: "bar",
-      orientation: "h"
-    };
+            // Trace1 for bar graph
+        var trace1 = {
+          x: sample_X,
+          y: otuIDS.map(i=> `otu ${i}`),
+          text: sampleValues.otu_labels.slice(0,10).reverse(),
+          name: "Top 10 OTUs",
+          type: "bar",
+          orientation: "h"
+        };
 
-    // data
-    var data = [trace1];
+      // data
+      var data = [trace1];
 
-    // Apply the group bar mode to the layout
-    var layout = {
-      title: "Top 10 OTUs for selected ID No",
-      margin: {
-        l: 100,
-        r: 100,
-        t: 100,
-        b: 100
-      }
-    };
+      // Apply the group bar mode to the layout
+      var layout = {
+        title: "Top 10 OTUs for selected ID No",
+        margin: {
+          l: 100,
+          r: 100,
+          t: 100,
+          b: 100
+        }
+      };
 
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("bar", data, layout);
+      // Render the plot to the div tag with id "plot"
+      Plotly.newPlot("bar", data, layout);
 
-         // })
+          // })
 
-    // The bubble chart
-    // x an y are not being picked up ********************
-    var trace1 = {
-      x: otuIDS,
-      y: sample_X,
-      mode: 'markers',
-      marker: {
-        size: sample_X,
-        color: otuIDS
-      },
-      text: prNames
+      // The bubble chart
+      // x an y are not being picked up ********************
+      var trace1 = {
+        x: otuIDS,
+        y: sample_X,
+        mode: 'markers',
+        marker: {
+          size: sample_X,
+          color: otuIDS
+        },
+        text: prNames
 
-    };
-    
-    var data = [trace1];
-    
-    var layout = {
-      xaxis:{title: "OTU ID"},
-      yaxis:{title: "Sample Values"},
-      title: 'Bacteria Quantities ',
-      showlegend: false,
-      height: 600,
-      width: 600
-    };
+      };
+      
+      var data = [trace1];
+      
+      var layout = {
+        xaxis:{title: "OTU ID"},
+        yaxis:{title: "Sample Values"},
+        title: 'Bacteria Quantities ',
+        showlegend: false,
+        height: 600,
+        width: 600
+      };
 
-    Plotly.newPlot('bubble', data, layout);
-  });               
+      Plotly.newPlot('bubble', data, layout);
+    });               
   
   
 
